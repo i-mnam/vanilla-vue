80e8a412fa1e4856faadfebeb02418b0c22d8897
i-mnam <8linkbv@gmail.com>
2021-04-12 01:39:34 +0900

    ch13.1 최근검색어

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,5 +1,6 @@
 import SearchModel from './models/SearchModel.js';
 import KeywordModel from './models/KeywordModel.js';
+import HistoryModel from './models/HistoryModel.js';
 
 new Vue({
     el: '#app',//vue ins가 html에 어느 부분에 mount 될 건지 정하는 것
@@ -10,12 +11,13 @@ new Vue({
         selectedTab: '', // !!!! 초기화의 중요성 view 쪽이 동적으로 작동하려면 초기화해두자
         tabs: ['추천 검색어', '최근 검색어'],
         keywords: [], // 추천 검색어 초기화
-
+        history: [], // 최근 검색어 초기화
     },
     created() {
         console.log('created()');// vue ins가 생성될 때, 호출되는 라이프사이클에 따라 호출되는 함수
         this.selectedTab = this.tabs[0];
         this.fetchKeyword();
+        this.fetchHistory();
     },
     methods: {
         onSubmit(e) {
@@ -55,6 +57,23 @@ new Vue({
             this.query = keyword;
             // this.onSubmit();
             this.search();
-        }
+        },
+        fetchHistory() {
+            console.log('fetchHistory()');
+            HistoryModel.list().then(data => {
+                this.history = data;
+            });
+        },
+        // onClickHistory(keyword) {
+        //     console.log('onClickHistory() keyword = ', keyword);
+        //     this.query = keyword;
+        //     this.search();
+        // }
+        // onRemoveHistory(keyword) {
+        //     console.log('onRemoveHistory()');
+        //     SearchModel.remove(keyword).list().then(data => {
+        //         this.history = data;
+        //     })
+        // }
     }
 });
